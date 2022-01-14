71f9f771879b6cf3db467ad391eccbbe0b7c7e59
ynzy <758922096@qq.com>
2022-01-15 00:03:45 +0800

    feat: 采用hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ interface IQueryParams {
 	code?: string
 }
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 	strict: true,
 	scrollBehavior: () => ({ left: 0, top: 0 })
